13ce99af009e53f22e22f07d432a0ad20c19e244
liuyibo-leo <63709525+liuyibo-leo@users.noreply.github.com>
2023-03-16 22:03:33 +0800

    Update 一维开心消消乐.py

--- Chapter 3 PekingU/OJ Test/一维开心消消乐.py
+++ Chapter 3 PekingU/OJ Test/一维开心消消乐.py
@@ -30,3 +30,8 @@ def crossOff(data):
 
 result = crossOff(input())
 print(''.join(result)) #join the items together with or the mark of '' in a list, like: ['b', 'p', 'x', 'y', 'z']
+
+
+#题目内容：开心消消乐我们都熟悉，我们可以用刚学过的栈来做一个“一维”的开心消消乐游戏，这个游戏输入一串字符，逐个消去相邻的相同字符对。如果字符全部被消完，则输出不带引号的“None”
+#输入格式:一个字符串，可能带有相邻的相同字符，如“aabbbc”
+#输出格式:一个字符串，消去了相邻的成对字符，如“bc”
